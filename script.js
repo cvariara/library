f08eb8c6cc618f55d5556a8d1f1ad6b998482dce
Christopher Variara <cvariara@gmail.com>
2023-09-11 12:38:47 -0400

    Added function to submit button
    When submit button is clicked, the book card is displayed on screen.
    Basic styling was added to card container and each card.

--- script.js
+++ script.js
@@ -3,22 +3,50 @@ const myLibrary = [
     title: "Red Rising",
     author: "Pierce Brown",
     pages: 382,
-    completed: true
-  }
+    completed: true,
+  },
+  {
+    title: "Golden Son",
+    author: "Pierce Brown",
+    pages: 434,
+    completed: false,
+  },
+  {
+    title: "Golden Son",
+    author: "Pierce Brown",
+    pages: 434,
+    completed: true,
+  },
+  {
+    title: "Golden Son",
+    author: "Pierce Brown",
+    pages: 434,
+    completed: false,
+  },
 ];
 
 const form = document.querySelector("form");
 const bookTitle = document.querySelector("#title");
 const bookAuthor = document.querySelector("#author");
 const bookPages = document.querySelector("#pages");
-const isBookCompleted = document.querySelector("#completed")
+const isBookCompleted = document.querySelector("#completed");
+const cards = document.querySelector(".cards");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  // const book = {};
+  // book.title = e.target.title.value;
+  // book.author = e.target.author.value;
+  // book.pages = e.target.pages.value;
+  // book.completed = e.target.completed.value;
+  // console.log(book)
+
   addBookToLibrary();
+  displayBooks();
 
   form.reset();
-})
+});
 
 function Book(title, author, pages, completed) {
   this.title = title;
@@ -28,12 +56,39 @@ function Book(title, author, pages, completed) {
 }
 
 function addBookToLibrary() {
-  if (bookTitle.value.length === 0 || bookAuthor.value.length === 0 || bookPages.value.length === 0) {
+  if (
+    bookTitle.value.length === 0 ||
+    bookAuthor.value.length === 0 ||
+    bookPages.value.length === 0
+  ) {
     alert("Please fill in all the fields appropriately.");
     return;
   }
 
-  const book = new Book(bookTitle.value, bookAuthor.value, pages.value, isBookCompleted.checked);
+  const book = new Book(
+    bookTitle.value,
+    bookAuthor.value,
+    pages.value,
+    isBookCompleted.checked
+  );
   myLibrary.push(book);
-  console.log(myLibrary);
 }
+
+function displayBooks(book) {
+  // console.log(book);
+  cards.innerHTML = "";
+  myLibrary.forEach((book) => {
+    cards.innerHTML += `
+      <div class="card">
+        <div class="title">Title: ${book.title}</div>
+        <div class="author">Author: ${book.author}</div>
+        <div class="pages">Pages: ${book.pages}</div>
+        <div class="completed">Completed?: ${book.completed ? "Yes" : "No"}</div>
+        <button class="remove-book">Remove Book</button>
+      </div>
+      `;
+  })
+}
+
+
+displayBooks();
